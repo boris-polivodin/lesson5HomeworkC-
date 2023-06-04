23174f1a92c0705858d19eba0b036a1f00c00360
Boris <89241300089@mail.ru>
2023-06-04 13:19:09 +0900

    Added Task2 and Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+class Program {
+  public static void Main(string[] args) {
+    int size = Prompt("Введите размер массива: ");
+    int minValue = Prompt("Введите нижнюю границу массива: ");
+    int maxValue = Prompt("Введите верхнюю границу массива: ");
+    int[] array = GetArray(size, minValue, maxValue);
+    Console.WriteLine($"Сумма нечетных элементов массива [{String.Join(", ", array)}] равна {ElementsSum(array)}");
+  }
+
+  static int Prompt(string message) {
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+  }
+
+  static int[] GetArray(int dim, int down, int up) {
+    int[] arr = new int[dim];
+    FillArray(arr, down, up);
+    return arr;
+  }
+
+  static void FillArray(int[] arr, int down, int up) {
+      Random rand = new Random();
+      for (int i = 0; i < arr.Length; i++)
+      {
+         arr[i] = rand.Next(down, up); 
+      }
+  }
+
+  static int ElementsSum(int[] arr) {
+    int oddElemensSum = 0;
+    for (int i = 0; i < arr.Length; i++) {
+      oddElemensSum += i % 2 == 0 ? 0 : arr[i];
+    }
+    return oddElemensSum;
+  }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+class Program {
+  public static void Main(string[] args) {
+    int size = Prompt("Введите размер массива: ");
+    int minValue = Prompt("Введите нижнюю границу массива: ");
+    int maxValue = Prompt("Введите верхнюю границу массива: ");
+    double[] array = GetArray(size, minValue, maxValue);
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{String.Join(", ", array)}] равна {DifferenceMinMax(array)}");
+  }
+  
+  static int Prompt(string message) {
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+  }
+
+  static double[] GetArray(int dim, int down, int up) {
+    double[] arr = new double[dim];
+    FillArray(arr, down, up);
+    return arr;
+  }
+    
+  static void FillArray(double[] arr, int down, int up) {
+      int dif = up - down;
+      Random rand = new Random();
+      for (int i = 0; i < arr.Length; i++)
+      {
+         arr[i] = Math.Round(rand.NextDouble() * ++dif + down, 2);
+      }
+  }
+
+  static double DifferenceMinMax(double[] arr) {
+    double minValue = arr[0];
+    double maxValue = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+      {
+        if (arr[i] < minValue) minValue = arr[i];
+        if (arr[i] > maxValue) maxValue = arr[i];
+      }
+    return Math.Round(maxValue - minValue, 2);
+  }
+}
